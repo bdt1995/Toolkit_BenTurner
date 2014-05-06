b9ffa43bda18d6cb2a641acfb3c9cc52890f5c78
bdt1995 <bdt1995@users.noreply.github.com>
2014-05-06 03:51:40 -0400

    Initial Commit
    Original Work, TODO: add more features, update with bin directory

---
+++ Toolbox/Toolbox/Form1.Designer.vb
@@ -0,0 +1,275 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Toolbox
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Toolbox))
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Button14 = New System.Windows.Forms.Button()
+        Me.Button13 = New System.Windows.Forms.Button()
+        Me.Button12 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Button10 = New System.Windows.Forms.Button()
+        Me.Button9 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.VerLabel = New System.Windows.Forms.Label()
+        Me.Button11 = New System.Windows.Forms.Button()
+        Me.Button15 = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Button14)
+        Me.GroupBox1.Controls.Add(Me.Button13)
+        Me.GroupBox1.Controls.Add(Me.Button12)
+        Me.GroupBox1.Controls.Add(Me.Button4)
+        Me.GroupBox1.Controls.Add(Me.Button3)
+        Me.GroupBox1.Controls.Add(Me.Button2)
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(249, 202)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Virus Removal"
+        '
+        'Button14
+        '
+        Me.Button14.Location = New System.Drawing.Point(7, 161)
+        Me.Button14.Name = "Button14"
+        Me.Button14.Size = New System.Drawing.Size(97, 26)
+        Me.Button14.TabIndex = 6
+        Me.Button14.Text = "ADW"
+        Me.Button14.UseVisualStyleBackColor = True
+        '
+        'Button13
+        '
+        Me.Button13.Location = New System.Drawing.Point(132, 115)
+        Me.Button13.Name = "Button13"
+        Me.Button13.Size = New System.Drawing.Size(99, 31)
+        Me.Button13.TabIndex = 5
+        Me.Button13.Text = "MBAR"
+        Me.Button13.UseVisualStyleBackColor = True
+        '
+        'Button12
+        '
+        Me.Button12.Location = New System.Drawing.Point(6, 115)
+        Me.Button12.Name = "Button12"
+        Me.Button12.Size = New System.Drawing.Size(98, 31)
+        Me.Button12.TabIndex = 4
+        Me.Button12.Text = "ComboFix"
+        Me.Button12.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(132, 70)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(99, 29)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = "RKILL"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(6, 70)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(99, 30)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "TDSS"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(130, 19)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(102, 26)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "SAS"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(6, 19)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(99, 27)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "MBAM"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Button15)
+        Me.GroupBox2.Controls.Add(Me.Button10)
+        Me.GroupBox2.Controls.Add(Me.Button9)
+        Me.GroupBox2.Controls.Add(Me.Button5)
+        Me.GroupBox2.Controls.Add(Me.Button6)
+        Me.GroupBox2.Controls.Add(Me.Button7)
+        Me.GroupBox2.Controls.Add(Me.Button8)
+        Me.GroupBox2.Location = New System.Drawing.Point(277, 12)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(249, 202)
+        Me.GroupBox2.TabIndex = 4
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Maintenence + Diagnostic"
+        '
+        'Button10
+        '
+        Me.Button10.Location = New System.Drawing.Point(132, 121)
+        Me.Button10.Name = "Button10"
+        Me.Button10.Size = New System.Drawing.Size(98, 25)
+        Me.Button10.TabIndex = 5
+        Me.Button10.Text = "MSCONFIG"
+        Me.Button10.UseVisualStyleBackColor = True
+        '
+        'Button9
+        '
+        Me.Button9.Location = New System.Drawing.Point(6, 121)
+        Me.Button9.Name = "Button9"
+        Me.Button9.Size = New System.Drawing.Size(98, 25)
+        Me.Button9.TabIndex = 4
+        Me.Button9.Text = "Services"
+        Me.Button9.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(132, 70)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(99, 29)
+        Me.Button5.TabIndex = 3
+        Me.Button5.Text = "Event Viewer"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(6, 70)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(99, 30)
+        Me.Button6.TabIndex = 2
+        Me.Button6.Text = "BSOD Viewer"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.Location = New System.Drawing.Point(130, 19)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(102, 26)
+        Me.Button7.TabIndex = 1
+        Me.Button7.Text = "Revo Uninstaller"
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button8
+        '
+        Me.Button8.Location = New System.Drawing.Point(6, 19)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(99, 27)
+        Me.Button8.TabIndex = 0
+        Me.Button8.Text = "CCleaner"
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(9, 310)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(158, 13)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Toolbox scripted by: Ben Turner"
+        '
+        'VerLabel
+        '
+        Me.VerLabel.AutoSize = True
+        Me.VerLabel.Location = New System.Drawing.Point(300, 310)
+        Me.VerLabel.Name = "VerLabel"
+        Me.VerLabel.Size = New System.Drawing.Size(60, 13)
+        Me.VerLabel.TabIndex = 7
+        Me.VerLabel.Text = "Version 1.6"
+        '
+        'Button11
+        '
+        Me.Button11.Location = New System.Drawing.Point(444, 305)
+        Me.Button11.Name = "Button11"
+        Me.Button11.Size = New System.Drawing.Size(75, 23)
+        Me.Button11.TabIndex = 8
+        Me.Button11.Text = "Update"
+        Me.Button11.UseVisualStyleBackColor = True
+        '
+        'Button15
+        '
+        Me.Button15.Location = New System.Drawing.Point(7, 161)
+        Me.Button15.Name = "Button15"
+        Me.Button15.Size = New System.Drawing.Size(97, 23)
+        Me.Button15.TabIndex = 6
+        Me.Button15.Text = "Add/Remove"
+        Me.Button15.UseVisualStyleBackColor = True
+        '
+        'Toolbox
+        '
+        Me.AccessibleRole = System.Windows.Forms.AccessibleRole.None
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(531, 337)
+        Me.Controls.Add(Me.Button11)
+        Me.Controls.Add(Me.VerLabel)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.DoubleBuffered = True
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Toolbox"
+        Me.Text = "Toolbox"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox2.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents Button10 As System.Windows.Forms.Button
+    Friend WithEvents Button9 As System.Windows.Forms.Button
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents Button6 As System.Windows.Forms.Button
+    Friend WithEvents Button7 As System.Windows.Forms.Button
+    Friend WithEvents Button8 As System.Windows.Forms.Button
+    Friend WithEvents Button12 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents VerLabel As System.Windows.Forms.Label
+    Friend WithEvents Button11 As System.Windows.Forms.Button
+    Friend WithEvents Button13 As System.Windows.Forms.Button
+    Friend WithEvents Button14 As System.Windows.Forms.Button
+    Friend WithEvents Button15 As System.Windows.Forms.Button
+
+End Class
---
+++ Toolbox/Toolbox/Form1.vb
@@ -0,0 +1,130 @@
+﻿Option Explicit On
+Public Class Toolbox
+
+
+    Private Declare Function GetTickCount Lib "kernel32" () As Long
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        run(".\bin\CCleaner.exe")
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        run(".\bin\Revouninstaller.exe")
+    End Sub
+
+    Private Sub Button12_Click(sender As Object, e As EventArgs) Handles Button12.Click
+        run(".\bin\Combofix.exe")
+
+    End Sub
+
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        run(".\bin\BlueScreenView.exe")
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        run(".\bin\MyEventViewer.exe")
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) Handles Button9.Click
+        run("services.msc")
+
+    End Sub
+
+    Private Sub Button10_Click(sender As Object, e As EventArgs) Handles Button10.Click
+        run("MSCONFIG")
+
+    End Sub
+
+
+    Function IsConnected() As Boolean
+        Return My.Computer.Network.Ping("www.google.com")
+
+    End Function
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If IsConnected() = True Then
+            run(".\bin\mbamnn.exe")
+        End If
+        If IsConnected() = False Then
+            run(".\bin\mbam.exe")
+        End If
+
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If IsConnected() = False Then
+            run(".\bin\SAS.exe")
+        End If
+        If IsConnected() = True Then
+            run(".\bin\SASNN.exe")
+        End If
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        run(".\bin\tdsskiller.exe")
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        run(".\bin\RKILL.exe")
+
+    End Sub
+
+    Private Sub Button13_Click(sender As Object, e As EventArgs)
+        run(".\bin\sfc.bat")
+    End Sub
+
+    Private Sub Button14_Click(sender As Object, e As EventArgs)
+        run(".\bin\chkdsk.bat")
+    End Sub
+
+
+
+
+    Private Sub Button11_Click(sender As Object, e As EventArgs) Handles Button11.Click
+        MsgBox("Updating... Please reopen application when done...")
+        run(".\bin\getnew.bat")
+        End
+
+    End Sub
+
+
+    Private Sub VerLabel_Click(sender As Object, e As EventArgs) Handles VerLabel.Click
+        MsgBox("Forcing update!")
+        run(".\bin\update.bat")
+        Wait(3)
+        run(".\bin\getnew.bat")
+        End
+    End Sub
+
+
+    Sub Wait(numSeconds As Long)
+        Dim start As Long
+        start = GetTickCount() + (numSeconds * 1000)
+
+        Do While start > GetTickCount()
+
+        Loop
+    End Sub
+
+    Private Sub Button13_Click_1(sender As Object, e As EventArgs) Handles Button13.Click
+        run(".\bin\mbar.exe")
+    End Sub
+
+    Private Sub Button14_Click_1(sender As Object, e As EventArgs) Handles Button14.Click
+        run(".\bin\adwcleaner.exe")
+    End Sub
+
+
+    Private Sub Button15_Click_1(sender As Object, e As EventArgs) Handles Button15.Click
+        run("appwiz.cpl")
+    End Sub
+    Private Sub run(A As String)
+        Try
+            System.Diagnostics.Process.Start(A)
+        Catch ex As Exception
+
+        End Try
+    End Sub
+End Class
